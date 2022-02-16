7f21a06b87bdcb6a7e9bc447c9914c22171a6c36
影子之城 <541616661@qq.com>
2022-02-16 02:41:38 +0000

    客户端去除租户拦截器

--- fantasy-oauth2/src/main/java/com/mzlalal/oauth2/dao/ClientDao.java
+++ fantasy-oauth2/src/main/java/com/mzlalal/oauth2/dao/ClientDao.java
@@ -1,5 +1,6 @@
 package com.mzlalal.oauth2.dao;
 
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mzlalal.base.entity.oauth2.dto.ClientEntity;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
  * @date 2021-07-28 15:47:17
  */
 @Mapper
+@InterceptorIgnore(tenantLine = "true")
 public interface ClientDao extends BaseMapper<ClientEntity> {
 
 }
